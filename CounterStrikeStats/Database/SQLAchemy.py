ee05be2b1937bd847b78cd050d5e8414625f1538
George Alton <georgealton@gmail.com>
2014-01-04 21:21:39 +0000

    Testing using the declarative base class for tables

--- CounterStrikeStats/Database/SQLAchemy.py
+++ CounterStrikeStats/Database/SQLAchemy.py
@@ -9,6 +9,9 @@ Created on 02 Jan 13
 #SQL alchemy
 from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.orm import mapper, sessionmaker
+from sqlalchemy.ext.declarative.api import declarative_base
+from sqlalchemy.schema import Column
+from sqlalchemy.types import *
 
 try:
     engine = create_engine(
@@ -24,6 +27,14 @@ try:
     Session = sessionmaker(bind=engine)
     session = Session()
     
+#http://docs.sqlalchemy.org/en/rel_0_9/orm/tutorial.html#declare-a-mapping
+#   
+# Classes mapped using the Declarative system are defined in terms of a 
+# base class which maintains a catalog of classes and tables relative to 
+# that base - this is known as the declarative base class. Our application 
+# will usually have just one instance of this base in a commonly imported 
+# module. We create the base class using the declarative_base() 
+    Base = declarative_base()
     
 except Exception as e:
     raise e
@@ -39,6 +50,17 @@ result = s.execute()
 
 for row in result:
     print row.lastName
+
+class Player(Base):
+    __tablename__ = 'hlstats_Player'
+    
+    id = Column(Integer, primary_key=True)
+    
+
+class Weapon(Base):
+    __tablename__ = "hlstats_Weapons"
+    
+    id = Column(Integer, primary_key=True)
     
     
     
